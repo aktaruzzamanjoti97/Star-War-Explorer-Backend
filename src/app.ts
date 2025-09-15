c819af95c82343f21b0ea5c4cdf81991e8f2020c
aktaruzzamanjoti97 <aktaruzzamanjoti97@gmail.com>
2025-09-15 21:29:52 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import cors from 'cors';
+import 'dotenv/config';
 import express, { Application, Request, Response } from 'express';
 import errorHandler from './middleware/errorHandler';
 import peopleRoutes from './routes/peopleRoutes';
 
 const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +17,9 @@ app.get('/', (_req: Request, res: Response) => {
 });
 
 app.use(errorHandler);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
